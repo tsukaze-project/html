5c889d7115eb06f72bbb51be94df8479ce031294
月風Project <tsukazeproject@gatsufuuProjectnoiMac.local>
2017-10-26 22:09:50 +0900

    18年待機調整

--- SimpleWPTheme/footer.php
+++ SimpleWPTheme/footer.php
@@ -1,4 +1,3 @@
 <footer>
-  <small>&copy; 2017 月風Project Official Web Site. All Rights Reserved. </small>
-
+  <small>&copy; 2018 月風Project Official Web Site. All Rights Reserved. </small>
 </footer>
